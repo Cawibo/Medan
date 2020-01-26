34aff0b171be7bf59a06f889851c45137b26d5da
Caroline <cawibo@gmail.com>
2020-01-26 11:07:31 +0100

    Add all instruction types.

--- medan/src/ast/syntax.rs
+++ medan/src/ast/syntax.rs
@@ -2,7 +2,28 @@ pub mod compiler {
 
     #[derive(Debug)]
     pub enum Code {
+        NOOP,
+
+        // data
         PUSH(usize),
         STORE(String),
+        FETCH(String),
+
+        // boolean constants
+        TRUE,
+        FALSE,
+
+        // operators
+        ADD,
+        MULT,
+        SUB,
+        EQ,
+        LE,
+        NEG,
+        AND,
+
+        // branching
+        BRANCH(Vec<Code>, Vec<Code>),
+        LOOP(Vec<Code>, Vec<Code>),
     }
 }
